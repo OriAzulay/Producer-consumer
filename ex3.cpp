84705bbfebfef8191455a7505d3e023dcbe39b19
oriAZul <ori101914@gmail.com>
2022-06-20 22:51:17 +0300

    printing

--- ex3.cpp
+++ ex3.cpp
@@ -12,10 +12,8 @@
 
 using namespace std;
 /*global variables:*/
-//queue array
-vector<queue<string>> queueLIst;
-//queue capacity
-int N = 10;
+vector<queue<string>> queueLIst;//queue array
+int N = 10;//queue capacity
 
 // This queue is FIFO : in ->>|4|3|2|1|-->out
 class BoundedQueue:public queue<string>{
@@ -45,7 +43,6 @@ class BoundedQueue:public queue<string>{
 	size_t getSize(){
 		return Qsize;
 	}
-
 };
 
 struct params
@@ -53,27 +50,42 @@ struct params
 	int id;
 	string type;
 	int numS;
-
 };
+
+string createNews(int i){
+	switch (i % 3)
+	{
+	case 1:
+		return " NEWS ";
+		break;
+	case 2:
+		return " SPORTS ";
+		break;
+	case 3:
+		return " WEATHER ";
+
+	}
+	return NULL;
+}
+
 
 void * producer(void* param){
 	//TODO : create string for Q here
 	params p = *(params*)param;
-	cout<<"producer number :"<< p.id<<endl;
 	BoundedQueue* q = new BoundedQueue(N);
+	string product = "produce " + to_string(p.id) + createNews(p.id) + to_string(p.numS);
 	//----test
-	q->push("producer number: " + to_string(p.id));
+	q->push(product);
 	//---
 	queueLIst.push_back(*q);
 	return NULL;	
 }
 
-
 //TODO: to see how insert queue size for each producer
 int main()
 {
 	cout<<"Start"<<endl;
-	int N = 10;
+	int N = 10; //size of producers
 	for(int i=0; i<N; i++){
 		params p;
 		p.id = i;
