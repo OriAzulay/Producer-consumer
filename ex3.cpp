b341098b291e54969ffd864d370fb3edfa24a7c1
oriAZul <ori101914@gmail.com>
2022-06-20 17:33:29 +0300

    producer

--- ex3.cpp
+++ ex3.cpp
@@ -13,13 +13,12 @@
 using namespace std;
 /*global variables:*/
 //queue array
-queue<string> * queueLIst;
+vector<queue<string>> queueLIst;
 //queue capacity
 int N = 10;
 
 // This queue is FIFO : in ->>|4|3|2|1|-->out
 class BoundedQueue:public queue<string>{
-
 	mutex m;
 	size_t Qsize;
 	public:
@@ -49,44 +48,40 @@ class BoundedQueue:public queue<string>{
 
 };
 
+struct params
+{
+	int id;
+	string type;
+	int numS;
+
+};
+
 void * producer(void* param){
 	//TODO : create string for Q here
-	int id = *(int*)param;
+	params p = *(params*)param;
+	cout<<"producer number :"<< p.id<<endl;
 	BoundedQueue* q = new BoundedQueue(N);
 	//----test
-	q->push("producer number: " + to_string(id));
+	q->push("producer number: " + to_string(p.id));
 	//---
-	queueLIst[id] = *q;
+	queueLIst.push_back(*q);
 	return NULL;	
 }
 
-void Producer(int id){
-	BoundedQueue* q = new BoundedQueue(N);
-	queueLIst[id] = *q;
-}
 
+//TODO: to see how insert queue size for each producer
 int main()
-
 {
 	cout<<"Start"<<endl;
 	int N = 10;
-	/*pthread_t*  t;
 	for(int i=0; i<N; i++){
-		int* a = (int*)malloc(sizeof(int)); // for passing a real value and not address of register
-		*a = i;
-		cout<<"log: create producer number"<<*a<<endl;
-		//TODO: to see how insert queue size for each producer
-		int temp = pthread_create(&t[i],NULL,producer,a);
+		params p;
+		p.id = i;
+		p.type;
+		pthread_t  t;
+		int temp = pthread_create(&t,NULL,producer,&p);
 	}
-	for(int i=0;i<queueLIst->size();i++){
-		cout<<queueLIst[i].front()<<endl;
-	}*/
-	
-	for (int i = 0; i < N ;  i++) {
-        std::thread t(Producer, i + 1);
-    }
-	for(int i=0;i<queueLIst->size();i++){
+	for(int i=0;i<queueLIst.size();i++){
 		cout<<queueLIst[i].front()<<endl;
 	}
-    
 }
